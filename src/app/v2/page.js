7da19ce0a2d49948f1b13a11a2a150adde66e9e1
ChangHangeol <hgchang1@naver.com>
2024-08-24 18:28:00 +0900

    [INIT]

---
+++ src/app/v2/page.js
@@ -0,0 +1,17 @@
+import DotV2 from "../../../components/flipdot/dotv2";
+
+
+export default function Page() {
+    
+    return (
+        <div
+            style={{
+                color : 'red',
+                disply : 'flex',
+                flexDirection : 'column',
+            }}    
+        >
+            <DotV2 />
+        </div>
+    )
+}
---
+++ test.md
@@ -0,0 +1,3 @@
+일부를 백엔드화 시켜서, 특정 url이 svg를 뱉도록 세팅해야 함.
+
+https://welcometodannas.tistory.com/82
